78f0af95559061737a8b029700ef2dfe21060132
Henry Gressmann <mail@henrygressmann.de>
2023-10-30 20:21:43 +0100

    chore: seo stuff
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [sitemap()],
+  site: "https://henrygressmann.de",
   compressHTML: true,
 });
