7a95f60d02dc1e39ff8fd6bf83f36622aecce9e3
Henry Gressmann <mail@henrygressmann.de>
2024-01-17 16:18:51 +0100

    update projects
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- src/components/Masonry.ts
+++ src/components/Masonry.ts
@@ -1,15 +1,15 @@
 declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'masonry-grid': MasonryGridProps;
-    }
-  }
+	namespace JSX {
+		interface IntrinsicElements {
+			"masonry-grid": MasonryGridProps;
+		}
+	}
 }
 
 export interface MasonryGridProps {
-  sm?: string;
-  md?: string;
-  lg?: string;
+	sm?: string;
+	md?: string;
+	lg?: string;
 }
 
 const TEMPLATE_STRING = `
@@ -47,84 +47,91 @@ const TEMPLATE_STRING = `
 `;
 
 class MasonryComponent extends HTMLElement {
-  #shadow: ShadowRoot;
-  #resizeObserver: ResizeObserver | null = null;
-  #columns: number = 0;
-  #grid: HTMLDivElement;
+	#shadow: ShadowRoot;
+	#resizeObserver: ResizeObserver | null = null;
+	#columns = 0;
+	#grid: HTMLDivElement;
 
-  constructor() {
-    super();
-    this.#shadow = this.attachShadow({ mode: 'open' });
+	constructor() {
+		super();
+		this.#shadow = this.attachShadow({ mode: "open" });
 
-    const template = document.createElement('template');
-    template.innerHTML = TEMPLATE_STRING;
+		const template = document.createElement("template");
+		template.innerHTML = TEMPLATE_STRING;
 
-    this.#shadow.appendChild(template.content.cloneNode(true));
-    this.#grid = this.shadowRoot!.querySelector<HTMLDivElement>("#grid")!;
-  }
+		this.#shadow.appendChild(template.content.cloneNode(true));
+		if (!this.shadowRoot) throw new Error("Shadow root not found");
+		const grid = this.shadowRoot.querySelector<HTMLDivElement>("#grid");
+		if (!grid) throw new Error("Grid not found");
+		this.#grid = grid;
+	}
 
-  connectedCallback() {
-    this.renderColumns();
-    this.#resizeObserver = new ResizeObserver(() => {
-      this.renderColumns();
-    });
-    this.#resizeObserver.observe(this);
-  }
+	connectedCallback() {
+		this.renderColumns();
+		this.#resizeObserver = new ResizeObserver(() => {
+			this.renderColumns();
+		});
+		this.#resizeObserver.observe(this);
+	}
 
-  disconnectedCallback() {
-    this.#resizeObserver?.disconnect();
-  }
+	disconnectedCallback() {
+		this.#resizeObserver?.disconnect();
+	}
 
-  static get observedAttributes() {
-    return ['sm', 'md', 'lg'];
-  }
+	static get observedAttributes() {
+		return ["sm", "md", "lg"];
+	}
 
-  attributeChangedCallback() {
-    this.renderColumns();
-  }
+	attributeChangedCallback() {
+		this.renderColumns();
+	}
 
-  renderColumns() {
-    const width = this.clientWidth;
-    let numColumns: number;
+	renderColumns() {
+		const width = this.clientWidth;
+		let numColumns: number;
 
-    let sm = parseInt(getComputedStyle(this).getPropertyValue('--breakpoint-sm'));
-    let md = parseInt(getComputedStyle(this).getPropertyValue('--breakpoint-md'));
+		const sm = parseInt(
+			getComputedStyle(this).getPropertyValue("--breakpoint-sm"),
+		);
+		const md = parseInt(
+			getComputedStyle(this).getPropertyValue("--breakpoint-md"),
+		);
 
-    if (width <= sm) {
-      numColumns = parseInt(this.getAttribute('sm') || "1");
-    } else if (width <= md) {
-      numColumns = parseInt(this.getAttribute('md') || "2");
-    } else {
-      numColumns = parseInt(this.getAttribute('lg') || "3");
-    }
+		if (width <= sm) {
+			numColumns = parseInt(this.getAttribute("sm") || "1");
+		} else if (width <= md) {
+			numColumns = parseInt(this.getAttribute("md") || "2");
+		} else {
+			numColumns = parseInt(this.getAttribute("lg") || "3");
+		}
 
-    if (numColumns === this.#columns) return;
+		if (numColumns === this.#columns) return;
 
-    // remove existing columns
-    while (this.#grid.firstChild) {
-      this.#grid.removeChild(this.#grid.firstChild);
-    }
+		// remove existing columns
+		while (this.#grid.firstChild) {
+			this.#grid.removeChild(this.#grid.firstChild);
+		}
 
-    for (let i = 0; i < numColumns; i++) {
-      const columnDiv = document.createElement('div');
-      const columnSlot = document.createElement(`slot`);
-      columnSlot.name = `column-${i}`;
-      columnDiv.append(columnSlot);
-      this.#grid.appendChild(columnDiv);
-    }
-    this.#columns = numColumns;
-    this.renderItems();
-  }
+		for (let i = 0; i < numColumns; i++) {
+			const columnDiv = document.createElement("div");
+			const columnSlot = document.createElement("slot");
+			columnSlot.name = `column-${i}`;
+			columnDiv.append(columnSlot);
+			this.#grid.appendChild(columnDiv);
+		}
+		this.#columns = numColumns;
+		this.renderItems();
+	}
 
-  renderItems() {
-    const children = Array.from(this.children);
+	renderItems() {
+		const children = Array.from(this.children);
 
-    for (const [i, child] of children.entries()) {
-      let columnIndex = i % this.#columns;
-      const column = `column-${columnIndex}`;
-      if (child.slot !== column) child.slot = column;
-    }
-  }
+		for (const [i, child] of children.entries()) {
+			const columnIndex = i % this.#columns;
+			const column = `column-${columnIndex}`;
+			if (child.slot !== column) child.slot = column;
+		}
+	}
 }
 
-customElements.define('masonry-grid', MasonryComponent);
+customElements.define("masonry-grid", MasonryComponent);
