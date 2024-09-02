aca59254a388fa52697ae082412f44fcf16064d6
Henry Gressmann <mail@henrygressmann.de>
2024-09-02 23:06:50 +0200

    chore: update projects
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

---
+++ src/projects.ts
@@ -0,0 +1,252 @@
+export const projects = [
+	{
+		title: "Open Source",
+		from: "2016-now",
+		text: "I've created and contributed to a lot of open source projects over the years. You can find all of them on my Github profile.",
+		buttons: [
+			{
+				link: "https://github.com/explodingcamera",
+				name: "Github",
+			},
+		],
+	},
+	{
+		title: "Freelance",
+		from: "2022-2024",
+		text: "I'm currently working as a freelance developer, working on a variety of projects. Main focus is on web development and React Native/Expo.",
+	},
+	{
+		title: "Liwan",
+		from: "2024-now",
+		img: "liwan.webp",
+		text: "Easy & privacy-first web analytics. Liwan is an open source web analytics platform that respects your users' privacy. No cookies, no cross-site tracking, no persistent identifiers. ",
+		buttons: [
+			{
+				link: "https://liwan.dev",
+				name: "liwan.dev",
+			},
+			{
+				link: "https://github.com/explodingcamera/liwan",
+				name: "Github",
+			},
+		],
+	},
+	{
+		title: "tinywasm",
+		from: "2023-now",
+		img: "tinywasm.png",
+		text: "A open-source WebAssembly runtime suitable for embedded systems and operating systems. Initially created as part of my University Capstone Project.",
+		buttons: [
+			{
+				link: "https://github.com/explodingcamera/tinywasm",
+				name: "Github",
+			},
+		],
+	},
+
+	{
+		title: "dawdle.space",
+		from: "2023-now",
+		img: "dawdle2.webp",
+		text: "dawdle.space is a community for sharing art, free software, and building fun websites. Includes a custom-built code editor and ssh proxy for running code.",
+		buttons: [
+			{
+				link: "https://dawdle.space",
+				name: "dawdle.space",
+			},
+		],
+	},
+	// {
+	// 	title: "StayGo",
+	// 	from: "2023-now",
+	// 	img: "staygo.webp",
+	// 	text: "canX StayGo helps you keep track of all your things and helps you to see if you really need them.\nI am currently working on the first version of the app.",
+	// 	buttons: [
+	// 		{
+	// 			link: "https://staygo.de/download",
+	// 			name: "App Store/Play Store",
+	// 		},
+	// 	],
+	// },
+	{
+		title: "Creating an Operating System in Rust",
+		from: "2023",
+		img: "os.webp",
+		text: "A series of blog posts guiding you through the process of creating an operating system in Rust",
+		buttons: [
+			{
+				link: "https://blog.henrygressmann.de/series/rust-os/",
+				name: "blog",
+			},
+			{
+				link: "https://github.com/explodingcamera/pogos",
+				name: "code",
+			},
+		],
+	},
+	// {
+	// 	title: "Keygate",
+	// 	from: "2023-now",
+	// 	img: "keygate.webp",
+	// 	text: "Modular and open-source identity management, authentication and authorization. Currently being rewritten with a focus on minimalism and security.",
+	// 	buttons: [
+	// 		{
+	// 			link: "https://keygate.io",
+	// 			name: "keygate.io",
+	// 		},
+	// 	],
+	// },
+	{
+		title: "Koi",
+		from: "2023",
+		img: "koi.webp",
+		text: "Koi is a new lossless image format that is designed for embedded systems and game engines",
+		buttons: [
+			{
+				link: "https://github.com/explodingcamera/koi-rs",
+				name: "code",
+			},
+			{
+				link: "https://blog.henrygressmann.de/koi",
+				name: "blog post",
+			},
+		],
+	},
+
+	{
+		title: "nots.dev",
+		from: "2023-now",
+		img: "nots.jpg",
+		text: "A open source self-hosted cloud platform — Just bring your code, and Nots will take care of the rest. Currently in development.",
+		buttons: [
+			{
+				link: "https://github.com/explodingcamera/nots",
+				name: "Github",
+			},
+			{
+				link: "https://nots.dev",
+				name: "nots.dev",
+			},
+		],
+	},
+	{
+		title: "pog.network",
+		from: "2021-2022",
+		img: "pog.network.webp",
+		text: "With POG.network we're striving to build a green and fast cryptocurrency. I'm the team lead, designed the protocol and created the architecture of the project",
+		buttons: [
+			{
+				link: "https://github.com/pognetwork",
+				name: "code",
+			},
+			{
+				link: "https://pog.network",
+				name: "pog.network",
+			},
+		],
+	},
+	{
+		title: "Snowstorm",
+		from: "2021-2022",
+		img: "snowstorm.webp",
+		text: "Snowstorm is a lightweight alternative to next.js, made possible by utilizing new technologies like ES modules and new build tools like es-build.\n\nFurther, Snowstorm is build from the ground up with React 18/Suspense in mind. It supports a broader ecosystem instead of building a walled garden.\n\nDiscontinued as the underlying bundler, Snowpack, is no longer actively maintained.",
+		buttons: [
+			{
+				link: "https://github.com/explodingcamera/snowstorm",
+				name: "code",
+			},
+			{
+				link: "https://snowstorm.js.org",
+				name: "snowstorm.js.org",
+			},
+		],
+	},
+	{
+		title: "Etournity",
+		from: "2020",
+		img: "etournity.webp",
+		text: "Etournity wants gamers to organize and participate in online esports tournaments effortlessly.\n\nI joined the Etournity team as a developer in the summer of 2020. My focus was on transitioning the project to next.js and securing the GraphQL API.",
+		buttons: [
+			{
+				link: "https://etournity.com",
+				name: "etournity.com",
+			},
+		],
+	},
+	{
+		title: "FantasyMarket",
+		from: "2020",
+		img: "fantasymarket.webp",
+		text: "FantasyMarket is a working stock market simulation where the user can invest in-game currency in a simulated market wholly separated from the real world.",
+		buttons: [
+			{
+				link: "https://github.com/fantasymarket",
+				name: "code",
+			},
+		],
+	},
+	{
+		title: "recordskip",
+		from: "2019",
+		img: "recordskip.jpeg",
+		text: "Recordskip is an app for vinyl record lovers tracks your collection with the help of Image Recognition and gives you music recommendations for expanding your musical horizons.",
+		buttons: [
+			{
+				link: "https://github.com/recordskip/recordskip-app",
+				name: "code",
+			},
+		],
+	},
+	{
+		title: "canX",
+		from: "2018 - 2020",
+		img: "canx-min.webp",
+		text: "I co-founded canX with the goal of creating a fairer platform for creators and their fans. Written in Go and React, with a custom CDN and video encoding pipeline.",
+		buttons: [
+			{
+				link: "https://canx.gmbh",
+				name: "canX.gmbh",
+			},
+		],
+	},
+	{
+		title: "livecount.pro",
+		from: "2017 - 2019",
+		img: "livecount-min.webp",
+		text: "LiveCount Pro is a real-time anaylytics platform and dashboard with support for multiple social networks and multiple channels simultaneously.",
+		buttons: [
+			{
+				link: "https://livecount.pro",
+				name: "livecount.pro",
+			},
+			{
+				link: "https://github.com/explodingcamera/livecount.pro",
+				name: "code",
+			},
+		],
+	},
+	{
+		title: "musiqpad",
+		from: "2016",
+		img: "musiqpad-min.webp",
+		text: "Musiqpad ia an open-source social-music software platform for users to host their own chatrooms and enjoy music together with others.\n\nI worked on modernizing the codebase and finding and fixing security vulnerabilities.",
+		buttons: [
+			{
+				link: "https://github.com/musiqpad/mqp-server",
+				name: "code",
+			},
+		],
+	},
+	{
+		title: "musiqplus",
+		from: "2015-2016",
+		img: "musiqplus.webp",
+		text: "MusiqPlus is a Google Chrome extension that enhanced the experience on musiqpad servers with a ton of extra features. This started my involvement with Musiqpad and was one of my first larger projects.",
+		buttons: [
+			{
+				link: "https://github.com/explodingcamera/musiqplus",
+				name: "code",
+			},
+		],
+	},
+];
